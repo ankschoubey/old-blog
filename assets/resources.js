548b631b2579bd0a9e90b21a68fc33ba0ea63d26
ankschoubey <ankschoubey@users.noreply.github.com>
2021-06-26 18:55:34 +0000

    deploy: e366aeecb1a3b59146cc719b92f05df685dbf902

--- assets/resources.js
+++ assets/resources.js
@@ -13,29 +13,46 @@ const getResources = ()=> {
 
 const getOne = (itemName, itemDetails) => {
     const outerDiv = document.createElement('div')
-
-    const titleDiv = document.createElement('h2')
-    titleDiv.innerText = itemName;
-    outerDiv.appendChild(titleDiv);
-
-    const dateDiv = document.createElement('h2')
-    dateDiv.innerText = itemDetails.date;
-    outerDiv.appendChild(dateDiv);
+    outerDiv.className = 'resource-item'
 
     if(itemDetails.image){
+        const imgRef = document.createElement('a')
+        imgRef.target= '_blank';
+        imgRef.href = itemDetails.url;
+        outerDiv.appendChild(imgRef);
+
         const img = document.createElement('img');
+        img.className = 'resource-img image-preview '
         img.src = itemDetails.image;
-        outerDiv.appendChild(img);
+        imgRef.appendChild(img);
     }
 
-    const linkDiv = document.createElement('a')
-    linkDiv.innerText = 'Link';
-    linkDiv.href = itemDetails.url;
-    outerDiv.appendChild(linkDiv);
+    const dataDiv = document.createElement('div')
+    dataDiv.className = 'resource-data'
+    outerDiv.appendChild(dataDiv);
+
+    const titleDiv = document.createElement('a')
+    titleDiv.className = 'resource-name'
+    titleDiv.target= '_blank';
+    titleDiv.innerText = itemName;
+    titleDiv.href = itemDetails.url;
+    dataDiv.appendChild(titleDiv);
+
+    const dateDiv = document.createElement('div')
+    dateDiv.className = 'resource-date post-meta'
+    dateDiv.innerText = itemDetails.date;
+    dataDiv.appendChild(dateDiv);
+
+    
+    const descDiv = document.createElement('div')
+    descDiv.className = 'resource-detail'
+    descDiv.innerText = itemDetails.detail;
+    dataDiv.appendChild(descDiv);
 
     const typeDiv = document.createElement('div')
     typeDiv.innerText = itemDetails.type;
-    outerDiv.appendChild(typeDiv);
+    typeDiv.className = 'resource-type tag fa fa-hashtag ' + itemDetails.type;
+    dataDiv.appendChild(typeDiv);
 
     return outerDiv;
 }
@@ -46,6 +63,7 @@ const renderThis = (data) => {
         holderDiv.removeChild(holderDiv.lastElementChild);
     }
     for (const [key, value] of Object.entries(data)) {
+        if(value.hidden){ continue; }
         holderDiv.appendChild(getOne(key, value));
     }
 }
